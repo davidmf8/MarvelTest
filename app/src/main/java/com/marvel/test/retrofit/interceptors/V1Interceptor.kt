9a89a92d5ac7d23868d117599d4962f480ee7e23
David Moreno <dmoreno.atsistemas@gmail.com>
2021-12-26 21:54:02 +0100

    Add MD5 encrypt for requests

--- app/src/main/java/com/marvel/test/retrofit/interceptors/V1Interceptor.kt
+++ app/src/main/java/com/marvel/test/retrofit/interceptors/V1Interceptor.kt
@@ -1,8 +1,10 @@
 package com.marvel.test.retrofit.interceptors
 
 import com.marvel.test.BuildConfig
+import com.marvel.test.extension.toMD5
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
+import java.security.MessageDigest
 
 fun getV1Interceptor(): Interceptor {
 
@@ -10,15 +12,23 @@ fun getV1Interceptor(): Interceptor {
 
         val newUrl: HttpUrl = chain.request().url
             .newBuilder()
+            .port(443)
+            .addQueryParameter("ts", BuildConfig.MARVEL_API_TS)
             .addQueryParameter(
                 "apikey",
-                BuildConfig.MARVEL_API_KEY
+                BuildConfig.MARVEL_PUBLIC_API_KEY
+            )
+            .addQueryParameter(
+                "hash",
+                (BuildConfig.MARVEL_API_TS + BuildConfig.MARVEL_PRIVATE_API_KEY + BuildConfig.MARVEL_PUBLIC_API_KEY).toMD5()
             )
             .build()
+
         // Headers
         val newRequest = chain.request()
             .newBuilder()
             .url(newUrl)
+            .addHeader("Content-Type", "application/json")
             .build()
 
         chain.proceed(newRequest)
